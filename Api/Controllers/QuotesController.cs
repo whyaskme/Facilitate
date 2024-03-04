5e05736dfb3c270470dacd3f968c75f07bd4a0c4
Joseph Baranauskas <joseph.baranauskas@gmail.com>
2024-03-04 14:24:37 -0600

    Adding MongoDB Atlas connection.

--- Api/Controllers/QuotesController.cs
+++ Api/Controllers/QuotesController.cs
@@ -1,5 +1,7 @@
 using Facilitate.Libraries.Models;
 using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using MongoDB.Bson;
 
 namespace Api.Controllers
 {
@@ -18,6 +20,24 @@ namespace Api.Controllers
         [HttpGet(Name = "GetQuotes")]
         public IEnumerable<Quote> GetQuotes()
         {
+            const string connectionUri = "mongodb+srv://facilitate:!13324BossWood@facilitate.73z1cne.mongodb.net/?retryWrites=true&w=majority&appName=Facilitate";
+            var settings = MongoClientSettings.FromConnectionString(connectionUri);
+            // Set the ServerApi field of the settings object to set the version of the Stable API on the client
+            settings.ServerApi = new ServerApi(ServerApiVersion.V1);
+            // Create a new client and connect to the server
+            var client = new MongoClient(settings);
+
+            // Send a ping to confirm a successful connection
+            try
+            {
+                var result = client.GetDatabase("admin").RunCommand<BsonDocument>(new BsonDocument("ping", 1));
+                Console.WriteLine("Pinged your deployment. You successfully connected to MongoDB!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
             List<Quote> quotes = new List<Quote>();
             int numQuotes = 5;
 
