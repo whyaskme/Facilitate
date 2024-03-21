4505929ae200e8c74deb5ca104db11ba5841a95b
Joseph Baranauskas <joseph.baranauskas@gmail.com>
2024-03-21 10:42:09 -0500

    Adding Mongo Identity

--- AdminBlazor/Program.cs
+++ AdminBlazor/Program.cs
@@ -1,6 +1,10 @@
 using AdminBlazor.Data;
 using AdminBlazor.Components;
-using static AdminBlazor.Components.Pages.Leads;
+
+using AspNetCore.Identity.Mongo;
+using AspNetCore.Identity.Mongo.Model;
+using MongoDB.Bson;
+using Microsoft.AspNetCore.Identity;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -19,6 +23,28 @@ builder.Services.AddSingleton<QuoteService>();
 builder.Services.AddCors(x => x.AddPolicy("externalRequests",
                     policy => policy
                 .WithOrigins("https://jsonip.com")));
+
+// At the ConfigureServices section in Startup.cs for MongoDB
+builder.Services
+    .AddIdentityCore<MongoUser>()
+    .AddRoles<MongoRole>()
+    .AddMongoDbStores<MongoUser, MongoRole, ObjectId>(mongo =>
+    {
+        mongo.ConnectionString = "mongodb://127.0.0.1:27017/Members";
+        // other options
+    })
+    .AddDefaultTokenProviders();
+
+builder.Services.AddIdentityMongoDbProvider<MongoUser, MongoRole>(identity =>
+{
+    identity.Password.RequiredLength = 8;
+    // other options
+},
+    mongo =>
+    {
+        mongo.ConnectionString = "mongodb://127.0.0.1:27017/Members";
+        // other options
+    });
 
 var app = builder.Build();
 
