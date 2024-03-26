cc9abdb273999e6c2a00eb1237b032cd39bcf4bf
Joseph Baranauskas <joseph.baranauskas@gmail.com>
2024-03-26 00:51:24 -0500

    Working on upload failures.

---
+++ AdminBlazor/Data/UploadController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdminBlazor.Data
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UploadController : ControllerBase
+    {
+        [HttpPost("[action]")]
+        public ActionResult Upload(IFormFile myFile)
+        {
+            try
+            {
+                // Write code that saves the 'myFile' file.
+                // Don't rely on or trust the FileName property without validation.
+                var tmpVal = "";
+            }
+            catch
+            {
+                return BadRequest();
+            }
+            return Ok();
+        }
+    }
+}
