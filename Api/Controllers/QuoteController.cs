d9d2829570102305ba2d291ec56208e50dc1208c
Joseph Baranauskas <joseph.baranauskas@gmail.com>
2024-04-14 18:23:30 -0500

    Refining quote controller

--- Api/Controllers/QuoteController.cs
+++ Api/Controllers/QuoteController.cs
@@ -2,7 +2,6 @@
 using Microsoft.AspNetCore.Mvc;
 using MongoDB.Driver;
 using MongoDB.Bson;
-using Api.Controllers;
 using static Facilitate.Libraries.Models.Constants.Messaging;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
