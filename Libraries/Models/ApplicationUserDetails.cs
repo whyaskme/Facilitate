2de22d398a77e8d4c004845e434c67225c3b1b20
Joseph Baranauskas <joseph.baranauskas@gmail.com>
2024-05-06 02:26:49 -0500

    Backup

---
+++ Libraries/Models/ApplicationUserDetails.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facilitate.Libraries.Models
+{
+    public class ApplicationUserDetails : ApplicationUser
+    {
+        public ApplicationUserDetails()
+        {
+            Roles = new List<string[]>();
+        }
+
+        public List<string[]> Roles { get; set; }
+    }
+}
