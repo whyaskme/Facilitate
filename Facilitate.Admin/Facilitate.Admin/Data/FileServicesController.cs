f713b2a33c4cbf0272b2ba110823d25bfb34eec7
Joseph Baranauskas <joseph.baranauskas@gmail.com>
2024-06-28 14:44:20 -0500

    Temp backup

---
+++ Facilitate.Admin/Facilitate.Admin/Data/FileServicesController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Facilitate.Admin.Data
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileServicesController : ControllerBase
+    {
+        // GET: api/<FileServicesController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<FileServicesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<FileServicesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+            var tmpVal = "";
+        }
+
+        // PUT api/<FileServicesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+            var tmpVal = "";
+        }
+
+        // DELETE api/<FileServicesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            var tmpVal = "";
+        }
+    }
+}
