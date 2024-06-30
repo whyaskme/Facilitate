4348cffa160fc1d3318330ba835279b6cc3640b4
Joseph Baranauskas <joseph.baranauskas@gmail.com>
2024-06-30 14:10:47 -0500

    Backup

--- Api/Controllers/FileServicesController.cs
+++ Api/Controllers/FileServicesController.cs
@@ -56,20 +56,92 @@ namespace Facilitate.Api.Controllers
             return resutMsg;
         }
 
-        // POST api/<FileServicesController>
         [HttpPost]
-        public string Post([FromBody] string value)
+        [Route("UploadFile")]
+        [DisableRequestSizeLimit]
+        public ActionResult UploadFile(IFormFile ImageUpload, string chunkMetadata)
         {
-            var fileId = ObjectId.GenerateNewId().ToString();
+            var rootPath = _hostingEnvironment.ContentRootPath;
+            //rootPath = _hostingEnvironment.WebRootPath;
 
-            return fileId;
+            var tempPath = Path.Combine(rootPath, "uploads");
+
+            // Removes temporary files
+            RemoveTempFilesAfterDelay(tempPath, new TimeSpan(0, 5, 0));
+
+            try
+            {
+                if (!string.IsNullOrEmpty(chunkMetadata))
+                {
+                    var metaDataObject = JsonSerializer.Deserialize<ChunkMetadata>(chunkMetadata);
+                    var tempFilePath = Path.Combine(tempPath, metaDataObject.FileGuid + ".tmp");
+                    if (!Directory.Exists(tempPath))
+                        Directory.CreateDirectory(tempPath);
+
+                    AppendContentToFile(tempFilePath, ImageUpload);
+
+                    if (metaDataObject.Index == (metaDataObject.TotalCount - 1))
+                    {
+                        ProcessUploadedFile(tempFilePath, metaDataObject.FileName);
+                        _fileUrlStorageService.Add(Guid.Parse(metaDataObject.FileGuid), @"uploads\" + metaDataObject.FileName);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest();
+            }
+            return Ok();
         }
 
-        // GET api/<FileServicesController>/5
-        //[HttpGet("{id}")]
-        //public string Get(int id)
-        //{
-        //    return "value";
-        //}
+        void RemoveTempFilesAfterDelay(string path, TimeSpan delay)
+        {
+            var dir = new DirectoryInfo(path);
+            if (dir.Exists)
+                foreach (var file in dir.GetFiles("*.tmp").Where(f => f.LastWriteTimeUtc.Add(delay) < DateTime.UtcNow))
+                    file.Delete();
+        }
+
+        void ProcessUploadedFile(string tempFilePath, string fileName)
+        {
+            var path = Path.Combine(_hostingEnvironment.WebRootPath, "Uploads");
+
+            var newFileName = "" + Guid.NewGuid().ToString() + " - " + fileName;
+
+            var imagePath = Path.Combine(path, newFileName);
+            if (System.IO.File.Exists(imagePath))
+            {
+                System.IO.File.Delete(imagePath);
+            }
+
+            System.IO.File.Copy(tempFilePath, imagePath);
+
+            // Cleanup
+            System.IO.File.Delete(tempFilePath);
+        }
+
+        void AppendContentToFile(string path, IFormFile content)
+        {
+            using (var stream = new FileStream(path, FileMode.Append, FileAccess.Write))
+            {
+                content.CopyTo(stream);
+            }
+        }
     }
+
+    // POST api/<FileServicesController>
+    //[HttpPost]
+    //public string Post([FromBody] string value)
+    //{
+    //    var fileId = ObjectId.GenerateNewId().ToString();
+
+    //    return fileId;
+    //}
+
+    // GET api/<FileServicesController>/5
+    //[HttpGet("{id}")]
+    //public string Get(int id)
+    //{
+    //    return "value";
+    //}
 }
