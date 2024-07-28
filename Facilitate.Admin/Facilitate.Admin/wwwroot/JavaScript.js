b076e77ad2836d2ad1f318f11dfbed1e2b5100c2
Joseph Baranauskas <joseph.baranauskas@gmail.com>
2024-07-27 22:30:48 -0500

    Backup

--- Facilitate.Admin/Facilitate.Admin/wwwroot/JavaScript.js
+++ Facilitate.Admin/Facilitate.Admin/wwwroot/JavaScript.js
@@ -1,5 +1,31 @@
 ﻿
-/*alert('Here...');*/
+//alert('Page load...');
+
+$(document).ready(function () {
+
+    //alert('DocReady...');
+
+    //var tradesList = document.getElementById('ddlTradesList');
+
+    //var elementCount = tradesList.childElementCount;
+
+    //alert('elementCount: ' + elementCount);
+
+    //if (tradesList == null)
+    //{
+    //    for (var i = 0; i < tradesList.childElementCount; i++) {
+
+    //    }
+
+    //    alert('Tradeslist found...');
+
+    //    var item = tradesList.FindItemByValue('Aggregate');
+
+    //    alert('item: ' + item);
+
+    //    tradesList.SetSelectedItem(item);
+    //}
+});
 
 function getPageReferrer()
 {
